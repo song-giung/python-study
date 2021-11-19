0698aeef1cc4748e2cc6fd7b39e5b085b600196b
song-giung <rldnddl87@gmail.com>
2021-11-19 22:52:14 +0900

    method vs classmethod vs staticmethod

--- class.py
+++ class.py
@@ -9,5 +9,48 @@ class Car:
     def __repr__(self):
         return f'{self.__class__.__name__}({self.color!r}, {self.mileage!r})'
 
+
 my_car = Car('red', 2314)
 print(repr(my_car))
+
+
+class MyClass:
+    def method(self):
+        return 'instance method called', self
+
+    @classmethod
+    def classmethod(cls):
+        return 'class method called', cls
+
+    @staticmethod
+    def staticmethod():
+        return 'static method called'
+
+
+myclass = MyClass()
+
+print(myclass.method())
+print(myclass.classmethod())
+print(myclass.staticmethod())
+
+
+##
+
+class Pizza:
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}' \
+               f'(' \
+               f'{self.ingredients!r}' \
+               f')'
+
+    # factory method
+    @classmethod
+    def margherita(cls):
+        return cls(['mozzarella', 'tomatoes'])
+
+    @classmethod
+    def prosciutto(cls):
+        return cls(['mozzarella', 'tomatoes', 'ham'])
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print('love python')
+
+
+class CountedObject:
+    num_instances = 0
+
+    def __init__(self):
+        self.__class__.num_instances += 1
+        # self.num_instances += 1 # 클래스 멤버가 아닌 인스턴스 멤버를 다시 선언하는 것(주의)
