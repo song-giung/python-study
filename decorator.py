38431005ad2d6ab02aaee537917fa25b625fd48a
song-giung <rldnddl87@gmail.com>
2021-11-19 18:15:06 +0900

    데코레이터 - 실행 순서

--- decorator.py
+++ decorator.py
@@ -1,15 +1,28 @@
-def uppercase(func):
+def strong(func):
     def wrapper():
-        original_result: str = func()
-        modified_result = original_result.upper()
-        return modified_result
+        return f'<strong>{func()}</strong>'
 
     return wrapper
 
 
-@uppercase
+def emphasis(func):
+    def wrapper():
+        return f'<em>{func()}</em>'
+
+    return wrapper
+
+
+@strong  # 2
+@emphasis  # 1
 def greet():
     return 'hello'
 
 
-print(greet())  # >>> HELLO
+def greet_with_no_decorator():
+    return 'hello'
+
+
+print(greet())  # <strong><em>hello</em></strong>
+
+decorated_greet = strong(emphasis(greet_with_no_decorator))
+print('no decorator : ' + decorated_greet())
