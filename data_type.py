b2822e1b57ad328b32687a8c13b6b949ec828601
song-giung <rldnddl87@gmail.com>
2021-11-20 13:37:06 +0900

    list

--- data_type.py
+++ data_type.py
@@ -1,21 +1,33 @@
+import array
 import collections
 from types import MappingProxyType
 
 od = collections.OrderedDict(one=1, two=2, three=2)
-print(od.keys())
+od.keys()
 
 dd = collections.defaultdict(list)
 dd['dogs'].append('Rufus')
 dd['dogs'].append('Kathrin')
-print(dd['dogs'])
 
 dict1 = {'one': 1, 'two': 2}
 dict2 = {'three': 3, 'four': 4}
 chain = collections.ChainMap(dict1, dict2)
-print(chain)
-print(chain['three'])
 
 writable = {'one': 1, 'two': 2}
 read_only = MappingProxyType(writable)
-read_only['one'] = 'new_value'
+# read_only['one'] = 'new_value'
 ## TypeError: 'mappingproxy' object does not support item assignment
+
+## list(가변 동적)
+arr = ['one', 'two', 'three']
+del arr[1]
+
+## tuple(불변)
+arr = ('one', 'two', 'three')
+# arr[0] = 'hello'  # error
+# del arr[0]  # error
+new_tuple = arr + (23,)  ## make copy
+
+arr = array.array('f', [1, 2, 3])
+arr.append(5)
+
