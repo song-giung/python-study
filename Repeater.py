61549c0a56dd3573461d265bb5de5f1c2de69a87
song-giung <rldnddl87@gmail.com>
2021-11-21 12:33:18 +0900

    iterator

--- Repeater.py
+++ Repeater.py
@@ -1,9 +1,28 @@
 class Repeater:
     def __init__(self, value):
         self.value = value
+        self.count = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        return self.value
+        if len(self.value) <= self.count:
+            raise StopIteration
+        new_value = self.value[self.count]
+        self.count += 1
+        return new_value
+
+
+my_list = [1, 2, 3]
+iterator = iter(my_list)
+
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))  # StopIteration 예외 발생
+
+
+repeater = Repeater('Hello')
+for next in repeater:
+    print(next)
