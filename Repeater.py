bacfea0f7a8cdecfc1622812204d104c1c544769
song-giung <rldnddl87@gmail.com>
2021-11-21 16:30:11 +0900

    generator expression

--- Repeater.py
+++ Repeater.py
@@ -1,28 +1,4 @@
-def repeat_three_times(value):
-    yield value
-    yield value
-    yield value
+iterator = ('Hello' for i in range(3)) # 제너레이텨 표현식
 
-
-iterator = repeat_three_times('hello')
-
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
-next(iterator)  # StopIteration 예외 발생
-
-
-def repeat_with_counter(value, max_repeats):
-    count = 0
-
-    while True:
-        if count >= max_repeats:
-            return
-
-        count += 1
-        yield value
-
-
-def repeat_with_counter_short(value, max_repeats):
-    for i in range(max_repeats):
-        yield value
+for x in iterator:
+    print(x)
