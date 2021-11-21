9b7961eb1afaa8ce11ad6ba696a8a313a29a42cf
song-giung <rldnddl87@gmail.com>
2021-11-21 16:18:16 +0900

    generator function

--- Repeater.py
+++ Repeater.py
@@ -1,28 +1,10 @@
-class Repeater:
-    def __init__(self, value):
-        self.value = value
-        self.count = 0
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if len(self.value) <= self.count:
-            raise StopIteration
-        new_value = self.value[self.count]
-        self.count += 1
-        return new_value
+def repeater(value):
+    while True:
+        yield value
 
 
-my_list = [1, 2, 3]
-iterator = iter(my_list)
-
-# print(next(iterator))
-# print(next(iterator))
-# print(next(iterator))
-# print(next(iterator))  # StopIteration 예외 발생
+print(repeater('Hey'))  # <generator object repeater at 0x101469120>
 
 
-repeater = Repeater('Hello')
-for next in repeater:
-    print(next)
+iterator = repeater('Hello')
+print(next(iterator))
