d5e00d1ec414960d24fd07468232eafe369a2f6f
song-giung <rldnddl87@gmail.com>
2021-11-21 16:24:49 +0900

    generator function

--- Repeater.py
+++ Repeater.py
@@ -1,10 +1,28 @@
-def repeater(value):
+def repeat_three_times(value):
+    yield value
+    yield value
+    yield value
+
+
+iterator = repeat_three_times('hello')
+
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+next(iterator)  # StopIteration 예외 발생
+
+
+def repeat_with_counter(value, max_repeats):
+    count = 0
+
     while True:
+        if count >= max_repeats:
+            return
+
+        count += 1
         yield value
 
 
-print(repeater('Hey'))  # <generator object repeater at 0x101469120>
-
-
-iterator = repeater('Hello')
-print(next(iterator))
+def repeat_with_counter_short(value, max_repeats):
+    for i in range(max_repeats):
+        yield value
