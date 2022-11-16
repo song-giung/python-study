029d5d226bb5825469b191be67e7391f2ac1e423
song-giung <rldnddl87@gmail.com>
2022-11-16 23:49:02 +0900

    threading

--- speed_up.py
+++ speed_up.py
@@ -1,17 +1,27 @@
 # concurrency
 
+import concurrent.futures
 import requests
 import time
+import threading
 
-def download_site(url: str, session: requests.Session):
+thread_local = threading.local()
+
+def get_session():
+    if not hasattr(thread_local, "session"):
+        thread_local.session = requests.Session()
+    return thread_local.session
+
+
+def download_site(url: str):
+    session = get_session()
     with session.get(url) as response:
         print(f"Read {len(response.content)} from {url}")
 
 
 def download_all_sites(sites:list[str]):
-    with requests.Session() as session:
-        for url in sites:
-            download_site(url, session)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(download_site, sites)
 
 
 sites = [
