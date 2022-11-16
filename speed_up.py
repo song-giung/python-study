ab3d907b866c0d842760294c640e6dcb0e51652b
song-giung <rldnddl87@gmail.com>
2022-11-16 23:59:18 +0900

    asyncio

--- speed_up.py
+++ speed_up.py
@@ -1,27 +1,24 @@
 # concurrency
 
-import concurrent.futures
-import requests
+import asyncio
+import aiohttp
 import time
-import threading
-
-thread_local = threading.local()
-
-def get_session():
-    if not hasattr(thread_local, "session"):
-        thread_local.session = requests.Session()
-    return thread_local.session
 
 
-def download_site(url: str):
-    session = get_session()
-    with session.get(url) as response:
-        print(f"Read {len(response.content)} from {url}")
+async def download_site(session, url: str):
+    async with session.get(url) as response:
+        print("Read {0} from {1}".format(response.content_length, url))
 
 
-def download_all_sites(sites:list[str]):
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        executor.map(download_site, sites)
+async def download_all_sites(sites:list[str]):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for url in sites:
+            task = asyncio.ensure_future(download_site(session, url))
+            tasks.append(task)
+        
+        await asyncio.gather(*tasks, return_exceptions=True)
+        
 
 
 sites = [
@@ -29,6 +26,6 @@ sites = [
         "http://olympus.realpython.org/dice",
     ] * 80
 start_time = time.time()
-download_all_sites(sites)
+asyncio.get_event_loop().run_until_complete(download_all_sites(sites))
 duration = time.time() - start_time
 print(f"Downloaded {len(sites)} in {duration} seconds")
